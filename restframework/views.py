f55ba4fd8ea9d52b60901628d683839079c651c9
talal-askus <talal@askussolution.tech>
2024-06-22 16:16:59 +0500

    22 june, REST Framework

---
+++ restframework/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+from REST.restframework.serializers import GroupSerializer, UserSerializer
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
